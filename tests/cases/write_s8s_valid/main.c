dd2f8b978577e02e5ba0dba49bbb4210cfa80dd5
siliconspecter <siliconspecter@outlook.com>
2025-01-26 23:15:54 +0000

    Fix incorrect assertion.

--- tests/cases/write_s8s_valid/main.c
+++ tests/cases/write_s8s_valid/main.c
@@ -12,5 +12,5 @@ void test()
   const int8_t expected_s8s[] = {0x71, -0x51, 0x62};
   assert_s8s(expected_s8s, actual_s8s, 3, "write_s8s valid s8s");
   uint8_t expected_buffer[] = {0x08, 0xe5, 0x71, 0xaf, 0x62, 0x1b};
-  assert_s8s(expected_buffer, actual_buffer, 6, "write_s8s valid buffer");
+  assert_u8s(expected_buffer, actual_buffer, 6, "write_s8s valid buffer");
 }
